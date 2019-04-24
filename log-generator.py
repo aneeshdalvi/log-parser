2e5aba054d9a842254078382f8243bec65768492
aneeshdalvi <adalvi1@asu.edu>
2019-04-24 11:48:12 -0700

    added new changes to generator and reader files

--- log-generator.py
+++ log-generator.py
@@ -4,17 +4,19 @@ import time
 import datetime
 import os
 
-
+os.mkdir('.\\testLogs')
 os.chdir('.\\testLogs')
 
 
 # getting cpus and random usage
 
-cpustr = '192.168.1.'
+cpustr = '192.168.'
 cpuid1 = 0
 cpuid2 = 1
-SERVERS = 10
-MINUTES_A_DAY = 20
+SERVERS = 1000
+MINUTES_A_DAY = 1440
+serverid = 1
+serverid1 = 0
 
 # start date initialized
 
@@ -24,19 +26,25 @@ MINUTES_A_DAY = 20
 
 
 # each server should generate a log every minute with 2 CPUs
-for serverid in range(SERVERS):
+for server in range(SERVERS):
     dateTime = datetime.datetime(2014, 10, 31, 0, 0, 0)
     # add a minute
     for minute in range(MINUTES_A_DAY):
         if dateTime.day == '1' and dateTime.month == '11':
             break
 
+        if serverid1 > 255:
+            serverid1 = 0
+            serverid += 1
+
         dateTime = dateTime + datetime.timedelta(minutes=1)
         unixtime2 = int(time.mktime(dateTime.timetuple()))
-        print(unixtime2)
+        # print(unixtime2)
 
-        f = open(cpustr + str(serverid) + ".log", "a+")
+        f = open(cpustr + str(serverid) + "." + str(serverid1) + ".log", "a+")
         randomUsage1 = random.randint(0, 100)
-        f.write(f'Timestamp: {unixtime2}, CPU: {cpustr + str(serverid)} , CPU_id: {cpuid1} , %usage: {randomUsage1}\n')
+        f.write(f'Timestamp: {unixtime2}, CPU: {cpustr + str(serverid) + "." + str(serverid1)} , CPU_id: {cpuid1} , %usage: {randomUsage1}\n')
         randomUsage2 = random.randint(0, 100)
-        f.write(f'Timestamp: {unixtime2}, CPU: {cpustr + str(serverid)} , CPU_id: {cpuid2} , %usage: {randomUsage2}\n')
+        f.write(f'Timestamp: {unixtime2}, CPU: {cpustr + str(serverid) + "." + str(serverid1)} , CPU_id: {cpuid2} , %usage: {randomUsage2}\n')
+
+    serverid1 += 1
--- log_reader.py
+++ log_reader.py
@@ -4,6 +4,7 @@ import datetime
 import time
 import sys
 
+# need to have this directory which have log files
 os.chdir('.\\testLogs')
 
 # take user input
@@ -24,8 +25,97 @@ while True:
             endtime = endtime.strip()
             # print(inputFile, cpuID, starttime, endtime)
 
-            # showing the user details as expected
-            print(f"CPU{int(cpuID)} usage on {inputFile}:")
+            try:
+
+                start_date_time = datetime.datetime.strptime(starttime, "%Y-%m-%d %H:%M")
+                end_date_time = datetime.datetime.strptime(endtime, "%Y-%m-%d %H:%M")
+                # print("date and time:", date_time.hour)
+
+                startdateTime = datetime.datetime(start_date_time.year, start_date_time.month, start_date_time.day, start_date_time.hour, start_date_time.minute)
+                startunixtime = int(time.mktime(startdateTime.timetuple()))
+                # print(startunixtime)
+
+                enddateTime = datetime.datetime(end_date_time.year, end_date_time.month, end_date_time.day, end_date_time.hour, end_date_time.minute)
+                endunixtime = int(time.mktime(enddateTime.timetuple()))
+                # print(endunixtime)
+
+                # check if cpuID is either 1 or 0
+                if cpuID != '1' and cpuID != '0':
+                    raise Exception
+
+                with open(inputFile + ".log", "r") as f:
+                    # showing the user details as expected
+                    print(f"CPU{int(cpuID)} usage on {inputFile}:")
+
+                    for line in f:
+                        line = ''.join(line)
+                        line = line.rstrip()
+
+                        line = line.split(',')
+                        # print(line)
+
+                        timestamp = line[0].split('Timestamp: ')
+                        timestamp = ''.join(timestamp)
+                        timestamp.strip()
+                        # print(timestamp)
+
+                        cpu = line[1].split(' CPU: ')
+                        cpu = ''.join(cpu)
+                        cpu.strip()
+                        # print(cpu)
+
+                        cpu_id = line[2].split(' CPU_id: ')
+                        cpu_id = ''.join(cpu_id)
+                        cpu_id.strip()
+                        # print(cpu_id)
+
+                        usage = line[3].split(' %usage: ')
+                        usage = ''.join(usage)
+                        usage.strip()
+                        # print(usage)
+
+                        # check start time and end time
+                        if startunixtime > endunixtime:
+                            raise Exception
+
+                        # check unixtime logic
+                        # check if timestamp is between start and end time
+                        if startunixtime <= int(timestamp) <= endunixtime:
+                            # print(startunixtime, timestamp, endunixtime)
+                            ans = {
+                                'timestamp': timestamp,
+                                'cpu_id': cpu_id,
+                                'usage': usage
+                            }
+
+                            # check for CPU id..
+                            if int(ans['cpu_id']) == int(cpuID):
+                                readableDate = datetime.datetime.fromtimestamp(int(ans['timestamp']))
+                                print(f"({readableDate}, {ans['usage']}%)", end=' ', flush=True)
+                    print()
+            except Exception:
+                print('\nError: Wrong input given. Please check your input values')
+        # if user clicks 'N'
+        else:
+            exit = input('\nPlease type "EXIT" to quit from this tool.. ')
+            if exit.lower() == 'exit':
+                sys.exit()
+            else:
+                pass
+    # for the first run
+    else:
+
+        inputFile = input('Enter ip address: ')
+        inputFile = inputFile.strip()
+        cpuID = input('Enter cpuID: ')
+        cpuID = cpuID.strip()
+        starttime = input('Enter start datetime: ')
+        starttime = starttime.strip()
+        endtime = input('Enter end datetime: ')
+        endtime = endtime.strip()
+        # print(inputFile, cpuID, starttime, endtime)
+
+        try:
 
             start_date_time = datetime.datetime.strptime(starttime, "%Y-%m-%d %H:%M")
             end_date_time = datetime.datetime.strptime(endtime, "%Y-%m-%d %H:%M")
@@ -39,7 +129,16 @@ while True:
             endunixtime = int(time.mktime(enddateTime.timetuple()))
             # print(endunixtime)
 
+            # check if cpuID is either 1 or 0
+            # print(cpuID != '1' or)
+            if cpuID != '1' and cpuID != '0':
+                raise Exception
+
             with open(inputFile + ".log", "r") as f:
+
+                # showing the user details as expected
+                print(f"CPU{int(cpuID)} usage on {inputFile}:")
+
                 for line in f:
                     line = ''.join(line)
                     line = line.rstrip()
@@ -67,6 +166,10 @@ while True:
                     usage.strip()
                     # print(usage)
 
+                    # check start time and end time
+                    if startunixtime > endunixtime:
+                        raise Exception
+
                     # check unixtime logic
                     # check if timestamp is between start and end time
                     if startunixtime <= int(timestamp) <= endunixtime:
@@ -82,77 +185,6 @@ while True:
                             readableDate = datetime.datetime.fromtimestamp(int(ans['timestamp']))
                             print(f"({readableDate}, {ans['usage']}%)", end=' ', flush=True)
                 print()
-        # if user clicks 'N'
-        else:
-            exit = input('Please type "EXIT" to quit from this tool. .')
-            if exit.lower() == 'EXIT':
-                sys.exit()
-            else:
-                pass
-    # for the first run
-    else:
-        inputFile = input('Enter ip address: ')
-        cpuID = input('Enter cpuID: ')
-        starttime = input('Enter start datetime: ')
-        endtime = input('Enter end datetime: ')
-        # print(inputFile, cpuID, starttime, endtime)
-
-        # showing the user details as expected
-        print(f"CPU{int(cpuID)} usage on {inputFile}:")
-
-        start_date_time = datetime.datetime.strptime(starttime, "%Y-%m-%d %H:%M")
-        end_date_time = datetime.datetime.strptime(endtime, "%Y-%m-%d %H:%M")
-        # print("date and time:", date_time.hour)
-
-        startdateTime = datetime.datetime(start_date_time.year, start_date_time.month, start_date_time.day, start_date_time.hour, start_date_time.minute)
-        startunixtime = int(time.mktime(startdateTime.timetuple()))
-        # print(startunixtime)
-
-        enddateTime = datetime.datetime(end_date_time.year, end_date_time.month, end_date_time.day, end_date_time.hour, end_date_time.minute)
-        endunixtime = int(time.mktime(enddateTime.timetuple()))
-        # print(endunixtime)
-
-        with open(inputFile + ".log", "r") as f:
-            for line in f:
-                line = ''.join(line)
-                line = line.rstrip()
-
-                line = line.split(',')
-                # print(line)
-
-                timestamp = line[0].split('Timestamp: ')
-                timestamp = ''.join(timestamp)
-                timestamp.strip()
-                # print(timestamp)
-
-                cpu = line[1].split(' CPU: ')
-                cpu = ''.join(cpu)
-                cpu.strip()
-                # print(cpu)
-
-                cpu_id = line[2].split(' CPU_id: ')
-                cpu_id = ''.join(cpu_id)
-                cpu_id.strip()
-                # print(cpu_id)
-
-                usage = line[3].split(' %usage: ')
-                usage = ''.join(usage)
-                usage.strip()
-                # print(usage)
-
-                # check unixtime logic
-                # check if timestamp is between start and end time
-                if startunixtime <= int(timestamp) <= endunixtime:
-                    # print(startunixtime, timestamp, endunixtime)
-                    ans = {
-                        'timestamp': timestamp,
-                        'cpu_id': cpu_id,
-                        'usage': usage
-                    }
-
-                    # check for CPU id..
-                    if int(ans['cpu_id']) == int(cpuID):
-                        readableDate = datetime.datetime.fromtimestamp(int(ans['timestamp']))
-                        print(f"({readableDate}, {ans['usage']}%)", end=' ', flush=True)
-            print()
-            flag += 1
+                flag += 1
+        except Exception:
+            print('\nError: Wrong Input given. Please check your input values')
